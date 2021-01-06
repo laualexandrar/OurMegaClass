5cd06f067e3f8ca6487bf0637e6f080334c32b26
Laura Roa <laualexandrar@gmail.com>
2021-01-05 22:41:28 -0800

    Practicing again and again

--- src/extraPractice/CrazyNumber.kt
+++ src/extraPractice/CrazyNumber.kt
@@ -1,24 +1,40 @@
 package extraPractice
 
 fun main() {
-   println( myCrazyCode(1))
+    println(sumDigitNumbers(1234))
 }
 
-fun myCrazyCode(input: Int): Int {
+fun sumDigitNumbers(input: Int):Int{
+
     var sum = 0
-    var num = input
+    var number = input
 
-
-    while (num >= 1) {
-
-        val reminder = num % 10
-        var quotient = num / 10
-        num = quotient
-        sum += reminder
-
+    while(number >= 1) {
+        val quotient = number / 10
+        val reminder = number % 10
+        sum = sum + reminder
+        number = quotient
     }
-
-//    println(sum)
-
     return sum
+
 }
+
+//fun main() {
+//   println( myCrazyCode(1234))
+//}
+//
+//fun myCrazyCode(input: Int): Int {
+//    var sum = 0
+//    var num = input
+//
+//    while (num >= 1) {
+//
+//        val reminder = num % 10
+//        var quotient = num / 10
+//        num = quotient
+//        sum += reminder
+//
+//    }
+////    println(sum)
+//    return sum
+//}
--- src/extraPractice/MyForPractice.kt
+++ src/extraPractice/MyForPractice.kt
@@ -1,19 +1,34 @@
 package extraPractice
 
 fun main() {
-    println(myNumber(arrayListOf(1, 2, 3, 4)))
+    println(myBeautifulList(arrayListOf(1,2,3,4)))
 }
 
-fun myNumber(myArray: ArrayList<Int>):Int {
-
-    //val myArray = arrayOf(1, 2, 3, 4)
-
+fun myBeautifulList(myCreativeList: ArrayList<Int>): Int {
     var sum = 0
-    for (number in myArray) {
-        sum = number + sum
 
-    }
+    for (number in myCreativeList) {
+        if (number >= 0) {
+            sum = number + sum
+        }
+    } // val sum =0  sum = number + sum ---> THIS IS WRONG I cannot reassign the val number it has to be var
     return sum
-
-
 }
+
+//fun main() {
+//    println(myNumber(arrayListOf(1, 2, 3, 4)))
+//}
+//
+//fun myNumber(myArray: ArrayList<Int>):Int {
+//
+//    //val myArray = arrayOf(1, 2, 3, 4)
+//
+//    var sum = 0
+//    for (number in myArray) {
+//        sum = number + sum
+//
+//    }
+//    return sum
+//
+//
+//}
